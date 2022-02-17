b284257842f7c0fc356983998bf2385051574821
tugot17 <piotr635@gmail.com>
2022-02-17 13:11:01 +0100

    Added multiple series annotation endpoint

--- app/endpoints/utils.py
+++ app/endpoints/utils.py
@@ -13,6 +13,44 @@ def get_square_mask(w, h, center_coordinates, radius, color=1):
     return mask
 
 
+def get_square_annotation_list(w, h, center_coordinates, radius):
+    color = 1
+
+    mask = np.zeros((w, h), dtype=np.uint8)
+    x_start, x_end = center_coordinates[0] - radius, center_coordinates[0] + radius
+    y_start, y_end = center_coordinates[1] - radius, center_coordinates[1] + radius
+
+    # print("x_start:", x_start, "x_end:", x_end, "y_start:", y_start, "y_end:", y_end)
+
+    mask[x_start : (x_end + 1), y_start] = color
+    mask[x_start : (x_end + 1), y_end] = color
+
+    mask[x_start, y_start : (y_end + 1)] = color
+    mask[x_end, y_start : (y_end + 1)] = color
+
+    return [[x_start, y_start], [x_end, y_start], [x_end, y_start], [x_end, y_end], [x_start, y_start]]
+
+    # return np.argwhere(mask == color).tolist()
+
+
+def mock_up_inference_annotation(
+    instances: List[FileDataset], radius_factor: float = 0.2
+):
+    """
+    A function that acts as a model inference mock-up. For each frame we assign a square mask
+    :param radius_factor:
+    :param instances:
+    :return:
+    """
+    w, h = instances[0].pixel_array.shape
+    radius = int((w + h) / 2 * radius_factor)
+    center_coordinates = (w // 2, h // 2)
+
+    return [
+        get_square_annotation_list(w, h, center_coordinates, radius) for _ in instances
+    ]
+
+
 def mock_up_inference(
     instances: List[FileDataset], radius_factor: float = 0.2
 ) -> np.ndarray:
@@ -23,7 +61,7 @@ def mock_up_inference(
     :return: mask of squares for each frame of shape (frames, w, h)
     """
     w, h = instances[0].pixel_array.shape
-    radius = int((w+h) / 2 * radius_factor)
+    radius = int((w + h) / 2 * radius_factor)
     center_coordinates = (w // 2, h // 2)
 
     return np.stack(
--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 import uvicorn
 from starlette.responses import RedirectResponse
 
-from app.endpoints import single_series_endpoint, whole_study_endpoint, multiple_series_endpoint
+from app.endpoints import (
+    single_series_endpoint,
+    whole_study_endpoint,
+    multiple_series_endpoint,
+    multiple_series_annotation_endpoint
+)
 
 import os
 
@@ -11,11 +16,31 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 def create_app():
     app = FastAPI()
+
+    app.include_router(
+        single_series_endpoint.router,
+        tags=["P"],
+        responses={404: {"description": "Not found"}},
+    )
+
+    app.include_router(
+        whole_study_endpoint.router,
+        tags=["P"],
+        responses={404: {"description": "Not found"}},
+    )
+
     app.include_router(
         multiple_series_endpoint.router,
         tags=["P"],
         responses={404: {"description": "Not found"}},
     )
+
+    app.include_router(
+        multiple_series_annotation_endpoint.router,
+        tags=["P"],
+        responses={404: {"description": "Not found"}},
+    )
+
     return app
 
 
